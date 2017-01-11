f951f4c3855722c31886169dd735e82c2fc92cbe
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-01-11 22:53:37 +0100

    Added "amount" parameter to GET api/users

--- Sources/App/Controllers API/UserController.swift
+++ Sources/App/Controllers API/UserController.swift
@@ -4,25 +4,43 @@ import FluentMySQL
 import Fluent
 
 final class UserController: ResourceRepresentable {
-    
-    func index(request: Request) throws -> ResponseRepresentable {
-        
-        let face = try User.query().filter("approved", .equals, true).sort("quality", .descending)
-        face.limit = Limit(count: 25, offset: 0)
-        return try face.all().makeJSON(request: request)
-    }
-
-    func makeResource() -> Resource<User> {
-        return Resource(
-            index: index
-        )
-    }
-    
+	
+	fileprivate let defaultAmount = 20
+	
+	func index(request: Request) throws -> ResponseRepresentable {
+		
+		let amount: Int
+		
+		if let amountParameter = request.data["amount"]?.int {
+			
+			guard amountParameter > 0 && amountParameter <= 20 else {
+				throw Abort.badRequest
+			}
+			
+			amount = amountParameter
+			
+		} else {
+			
+			amount = defaultAmount
+			
+		}
+		
+		let face = try User.query().filter("approved", .equals, true).sort("quality", .descending)
+		face.limit = Limit(count: amount, offset: 0)
+		return try face.all().makeJSON(request: request)
+	}
+	
+	func makeResource() -> Resource<User> {
+		return Resource(
+			index: index
+		)
+	}
+	
 }
 
 extension Request {
-    func user() throws -> User {
-        guard let json = json else { throw Abort.badRequest }
-        return try User(node: json)
-    }
+	func user() throws -> User {
+		guard let json = json else { throw Abort.badRequest }
+		return try User(node: json)
+	}
 }
