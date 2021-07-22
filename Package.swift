ddc6a7c998e36dbe66f2957f74bfc08d74bc2721
Maxime De Greve <maximedegreve@me.com>
2021-07-22 09:23:56 +0200

    Remove

--- Package.swift
+++ Package.swift
@@ -1,16 +1,38 @@
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
-    name: "MarvelFaces",
-    dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/jhonny-me/CorsMiddleware.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/twostraws/SwiftGD.git", majorVersion: 1, minor: 1)
+    name: "leaderboard",
+    platforms: [
+       .macOS(.v10_15)
     ],
-    exclude: [
-        "Database",
-        "Localization",
-        "Tests",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.36.2"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.1.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(name: "QueuesFluentDriver", url: "https://github.com/m-barthelemy/vapor-queues-fluent-driver.git", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "QueuesFluentDriver", package: "QueuesFluentDriver")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-production> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .target(name: "Run", dependencies: [.target(name: "App")]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
     ]
 )
