d37d99f28812262e03c4a29938a88b9b7b221c22
Maxime De Greve <maximedegreve@me.com>
2021-07-24 11:29:46 +0200

    Fix size

--- Sources/App/Controllers/HomeController.swift
+++ Sources/App/Controllers/HomeController.swift
@@ -6,11 +6,16 @@ final class HomeController {
     func index(request: Request) throws -> EventLoopFuture<View> {
 
         struct HomeContext: Encodable {
-            var avatars: [Avatar]
+            var avatars: [String]
         }
 
-        return Avatar.query(on: request.db).filter(\.$approved == true).sort(\.$quality, .descending).all().flatMap { avatars in
-            return request.view.render("home", HomeContext(avatars: avatars))
+        return Avatar.query(on: request.db).filter(\.$approved == true).sort(\.$quality, .descending).limit(42).all().flatMap { avatars in
+            
+            let urls = avatars.map { avatar in
+                return avatar.url.replacingOccurrences(of: "/image/upload/", with: "/image/upload/w_300,h_300,c_fit/", options: .literal, range: nil)
+            }
+            
+            return request.view.render("home", HomeContext(avatars: urls))
         }
 
     }
