4be9337338ce988d1f7f23c4ac2a414b086e3ddf
Maxime De Greve <maximedegreve@me.com>
2021-07-25 08:28:34 +0100

    Add first name and last name

--- Sources/App/Models/Public/PublicAvatar.swift
+++ Sources/App/Models/Public/PublicAvatar.swift
@@ -6,13 +6,17 @@ final class PublicAvatar: Content {
     var source: PublicSource
     var url: String
     var gender: Gender
+    var firstName: String
+    var lastName: String
     var approved: Bool
     var createdAt: Date?
     var updatedAt: Date?
 
-    init(avatar: Avatar) {
+    init(avatar: Avatar, firstName: String, lastName: String) {
         self.id = avatar.id
         self.source = PublicSource(source: avatar.source)
+        self.firstName = firstName
+        self.lastName = lastName
         self.url = avatar.url
         self.gender = avatar.gender
         self.approved = avatar.approved
@@ -25,6 +29,8 @@ final class PublicAvatar: Content {
         case source = "source"
         case url = "url"
         case gender = "gender"
+        case firstName = "first_name"
+        case lastName = "last_name"
         case approved = "approved"
         case createdAt = "created_at"
         case updatedAt = "updated_at"
