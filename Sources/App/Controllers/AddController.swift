11052204b1411fb3fa5d795a4736c3d1578496ec
Maxime De Greve <maximedegreve@me.com>
2021-09-02 15:29:47 +0300

    Handle genders ourselves instead of depending on FB's review process (#15)
    * Start adding a gender selector as FB API keeps making problems about it
    
    * Gender
    
    * Get ready for selector
    
    * Remove gender all together

--- Sources/App/Controllers/AddController.swift
+++ Sources/App/Controllers/AddController.swift
@@ -5,11 +5,24 @@ final class AddController {
 
     func index(request: Request) throws -> EventLoopFuture<View> {
 
+        struct GenderValue: Encodable {
+            var value: String
+            var name: String
+        }
+        
         struct AddContext: Encodable {
             var facebookAppId: String
+            var genders: [GenderValue]
         }
 
-        return request.view.render("add", AddContext(facebookAppId: Environment.facebookAppId))
+        let genderValues = Gender.allCases.map{$0.rawValue}
+
+        let genders: [GenderValue] = genderValues.map { genderValue in
+            let name = genderValue.replacingOccurrences(of: "_", with: " ").capitalized
+            return GenderValue(value: genderValue, name: name)
+        }
+        
+        return request.view.render("add", AddContext(facebookAppId: Environment.facebookAppId, genders: genders))
 
     }
 
